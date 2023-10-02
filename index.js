e533c9ed3ccbc9f1a098add3dbde65b259af0840
Josiah <jhester911@gmail.com>
2023-10-02 09:33:40 -0400

    Updated the index.html and tip.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-import * as http from 'node:http';
-import * as fs from 'node:fs/promises';
+import * as http from 'http';
+import * as fs from 'fs';
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   fs.readFile('index.html', function(err, data) {
+  if (err) throw err;
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(data);
   return res.end();
-  });
+  })
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
